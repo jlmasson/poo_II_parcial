93cb5e3a957c09141666e2179d8ae635efca6d7b
daniloti1 <danilo.ti@hotmail.com>
2016-08-14 18:14:03 -0500

    creacion de proyecto

---
+++ Proyecto2Poo/src/Jugador.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class Jugador {
+    private String nickname;
+    private int puntos;
+    private int vidas;
+    
+    public Jugador(String nombre) {
+        this.nickname = nombre;
+        this.puntos = 0;
+        this.vidas = 3;
+    }
+    
+    
+}
