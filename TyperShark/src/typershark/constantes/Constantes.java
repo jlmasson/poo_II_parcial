a2540f6255fa1ee157c65a3110440c821580c24b
José Luis Massón <jlmasson@espol.edu.ec>
2016-08-20 21:02:32 -0500

    Adjunto constantes de animales
    Pilas, aqui estan las constantes.

---
+++ TyperShark/src/typershark/constantes/Constantes.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package typershark.constantes;
+
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author Jose Masson
+ */
+public class Constantes {
+    
+    public static final Color COLOR_INICIAL = Color.WHITE;
+    public static final Color COLOR_FINAL = Color.YELLOW;
+    public static final int AUMENTO_VELOCIDAD = 50;
+    public static final double MULTIPLICADOR_TIBURON = 1;
+    public static final double MULTIPLICADOR_PIRANIA = 2;
+    public static final int NIVEL_MAXIMO = 5;
+    public static final int PUNTOS_PODER = 400;
+    public static final int PUNTOS_TIBURON = 25;
+    public static final int PUNTOS_PIRANIA = 10;
+    public static final int PUNTOS_TIBURON_NEGRO = 75;
+    public static final int DISMINUCION_ALTURA_BUCEADOR = 15;
+    
+}
