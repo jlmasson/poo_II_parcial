ff010cd2e904546be76f03a9c306ca6c9cbb3639
daniloti1 <danilo.ti@hotmail.com>
2016-08-22 21:23:47 -0500

    Quitar vida a jugador (Piranha)

--- TyperShark/src/typershark/animals/Piranha.java
+++ TyperShark/src/typershark/animals/Piranha.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javafx.scene.image.Image;
 import typershark.constantes.ConstantesPuntos;
+import typershark.panels.Mar;
 import typershark.people.Jugador;
 
 /**
@@ -18,8 +19,8 @@ import typershark.people.Jugador;
 public class Piranha extends AnimalMarino {
     private char letra;
     
-    public Piranha(ArrayList<String> palabrasJuego, long velocidad) {
-        super("images/components/piranha.png",palabrasJuego, velocidad);
+    public Piranha(Mar mar, ArrayList<String> palabrasJuego, long velocidad) {
+        super(mar, "images/components/piranha.png",palabrasJuego, velocidad);
         Random r = new Random();
         //int aleatorio = r.nextInt(palabrasJuego.size());
         
@@ -38,5 +39,23 @@ public class Piranha extends AnimalMarino {
 
     public void agregarTextos() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void quitarVidas(Jugador jugador) {
+        if (this.getRoot().getLayoutX() == 0 && !this.isAlive()) {
+            if (jugador.getAtaquesDePiranhas() % 3 == 0 && jugador.getAtaquesDePiranhas() > 0) {
+                jugador.setAtaquesDePiranhas(0);
+                jugador.setNumVidas(jugador.getNumVidas() - 1);
+            }
+            else {
+                jugador.setAtaquesDePiranhas(jugador.getAtaquesDePiranhas() + 1);
+            }
+        }
+    }
+    
+    @Override
+    public void matarAnimal() {
+        super.matarAnimal();
     }
 }
