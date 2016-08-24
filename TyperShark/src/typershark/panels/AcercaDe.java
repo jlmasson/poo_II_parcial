ad01d22d9725120d7b9458339312f6f2e555169b
gaalocastillo <gadacast@espol.edu.ec>
2016-08-24 07:28:38 -0500

    Documentación
    Documenté AcercaDe y Jugador

--- TyperShark/src/typershark/panels/AcercaDe.java
+++ TyperShark/src/typershark/panels/AcercaDe.java
@@ -1,8 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* @(#)Tiburon.java 4.0 28/8/2016
+*
+* Copyright (c) 2016 Galo Castillo, Jose Luis Masson & Danilo Torres.
+* Escuela Superior Politécnica del Litoral. Guayaquil, Ecuador.
+* Todos los Derechos Reservados.
+*/
+
+
+
 package typershark.panels;
 
 import javafx.geometry.Pos;
@@ -13,29 +18,47 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 
+
 /**
- *
- * @author Jose Masson
+ * Esta clase define objetos de tipo Buceador.
+ * @author: Galo Castillo, Jose Luis Masson, Danilo Torres
+ * @version: 4.0 28/8/2016
  */
 public class AcercaDe {
+    /** Panel raiz de AcercaDe*/
     private BorderPane root;
     
+    /**
+     * Constructor para el objeto de tipo AcercaDe.
+     * Crea un nuevo panel en el que hay un boton y un campo de texto con puntos y numero de vidas iniciales.
+     */
     public AcercaDe() {
         this.root = new BorderPane();
         this.root.getStylesheets().add("styles/styles.css");
         this.root.getStyleClass().add("acercaDe");
         this.setupTitlePane();
         this.setupText();
-    }
+    }// Cierre constructor.
 
+    
+    /**
+     * Método que permite obtener el panel raiz.
+     * */
     public BorderPane getRoot() {
         return root;
-    }
+    } //Cierre del metodo.
 
+    /**
+     * Método que permite cambiar el panel raiz.
+     * @param root El parametro root es la nueva raiz.
+     * */
     public void setRoot(BorderPane root) {
         this.root = root;
-    }
+    } //Cierre del metodo
     
+    /**
+     * Método que crea y edita el formato del título de AcercaDe.
+     * */
     private void setupTitlePane() {
         VBox title;
         title = new VBox();
@@ -49,8 +72,11 @@ public class AcercaDe {
         titlePane.setTextAlignment(TextAlignment.CENTER);
         title.getChildren().add(titlePane);
         this.root.setTop(title);
-    }
+    }//Cierre del metodo.
     
+    /**
+     * Método que muestra la información en el panel AcercaDe.
+     * */
     private void setupText() {
         VBox center;
         center = new VBox();
@@ -77,14 +103,17 @@ public class AcercaDe {
         
         center.getChildren().addAll(espol, poo, elaborado);
         this.root.setCenter(center);
-    }
+    }//Cierre del metodo.
     
+    /**
+     * Método que cambia el formato de la fuente en el panel AcercaDe.
+     * */
     private DropShadow setupShadow() {
         DropShadow ds = new DropShadow();
         ds.setOffsetY(5.0f);
         ds.setOffsetX(-5.0f);
         ds.setColor(Color.color(0,0,0));
         return ds;
-    }
+    }//Cierre del metodo.
     
-}
+}//Cierre de la clase.
--- TyperShark/src/typershark/people/Jugador.java
+++ TyperShark/src/typershark/people/Jugador.java
@@ -1,8 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* @(#)Tiburon.java 4.0 28/8/2016
+*
+* Copyright (c) 2016 Galo Castillo, Jose Luis Masson & Danilo Torres.
+* Escuela Superior Politécnica del Litoral. Guayaquil, Ecuador.
+* Todos los Derechos Reservados.
+*
+*/
 package typershark.people;
 
 import java.io.File;
@@ -11,64 +14,107 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
- *
- * @author Danilo Torres
+ * Esta clase define objetos de tipo Buceador.
+ * @author: Galo Castillo, Jose Luis Masson, Danilo Torres
+ * @version: 4.0 28/8/2016
  */
 public class Jugador implements Comparable<Jugador>{
+    /** Sobrenombre que recibe el jugador para identificarlo*/
     private String nickname;
+    /** Puntaje obtenido por el jugador a lo largo del juego.*/
     private int puntos;
+    /** Numero de ataques de tiburones necesarios para perder. En el caso de 
+     las piranhas son numVidas x 3*/
     private int numVidas;
+    /** Acumulador de ataques de piranhas. Se pierde una vida por cada trees, 
+     * .*/
     private int ataquesDePiranhas;
     
+    /**
+     * Constructor para el objeto de tipo Jugador.
+     * Crea un nuevo jugador con puntos y numero de vidas iniciales.
+     * @param nickname El parámetro nickname utilizado a lo largo del juego
+     * para ser utilizado como identificador de este en el juego.
+     */
     public Jugador(String nickname) {
         this.nickname = nickname;
         this.puntos = 10000;
         this.numVidas = 3;
         this.ataquesDePiranhas = 0;
-    }
+    } //Cierre del constructor
 
+    /**
+     * Constructor para el objeto de tipo Jugador.
+     * Crea un nuevo jugador con puntos y numero de vidas iniciales.
+     * @param nickname El parámetro nickname utilizado a lo largo del juego
+     * para ser utilizado como identificador de este en el juego.
+     * @param puntos Numero de puntos iniciales del jugador.
+     */
     public Jugador(String nickname, int puntos) {
         this.nickname = nickname;
         this.puntos = puntos;
-    }
+    } // Cierre del constructor
     
+    /**
+     * Método que permite obtener el nickname del usuario.
+     * */
     public String getNickname() {
         return nickname;
-    }
+    } //Cierre del metodo.
 
+    
+    /**
+     * Método que permite cambir el nickname del usuario.
+     * @param nickname El parametro nickname es el nuevo sobrenombre que toma
+     * el jugador.
+     * */
     public void setNickname(String nickname) {
         this.nickname = nickname;
-    }
+    }//Cierre del metodo.
 
+    /**
+     * Método que permite obtener los puntos ganados por del usuario.
+     * */
     public int getPuntos() {
         return puntos;
-    }
+    }//Cierre del metodo.
 
     public void setPuntos(int puntos) {
         this.puntos = puntos;
-    }
+    }//Cierre del metodo.
 
-    public int getNumVidas() {
-        return numVidas;
+        /**
+     * Método que permite obtener el numero de vidas del usuario.
+     * */    public int getNumVidas() {
+        return numVidas;//Cierre del metodo.
     }
 
     public void setNumVidas(int numVidas) {
-        this.numVidas = numVidas;
-    }
+        this.numVidas = numVidas;//Cierre del metodo.
+    }//Cierre del metodo.
 
+    /**
+     * Método que permite obtener el numero de ataques de pirañas..
+     * */
     public int getAtaquesDePiranhas() {
         return ataquesDePiranhas;
-    }
+    }//Cierre del metodo.
 
+    /**
+     * Método que permite obtener el nickname del usuario.
+     * @param  ataquesDePiranhas El metodo ataqueDePiranhas es utilizado para contar
+     * si ya han atacadas las piarañas ncearas para que se uba nizacion ();
+     * */
     public void setAtaquesDePiranhas(int ataquesDePiranhas) {
         this.ataquesDePiranhas = ataquesDePiranhas;
-    }
+    }//Cierre del metodo.
 
     @Override
     public boolean equals(Object o) {
         Jugador j = (Jugador) o;
         return (this.nickname.equals(j.nickname) && this.puntos == j.puntos);
-    }
+    }//Cierre del metodo.
+    
     
     @Override
     public int compareTo(Jugador o) {
@@ -80,8 +126,11 @@ public class Jugador implements Comparable<Jugador>{
         else {
             return 1;
         }
-    }
+    }//Cierre del metodo.
     
+    /**
+     * Método estático que permite cargar jugadores segun sus pntoso.
+     * */
     public static LinkedList<Jugador> cargarJugadorXPuntos() {
         LinkedList<Jugador> jugadores = new LinkedList<>();
         File archivo = new File("src/puntajes/puntajes.txt");
@@ -100,8 +149,8 @@ public class Jugador implements Comparable<Jugador>{
             }
         }
         return jugadores;
-    }
+    }//Cierre del metodo.
     
     
     
-}
+}//Cierre de la clase.
